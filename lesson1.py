e48ba31adedc4ad89d81b6cef70ce0855f6a69a6
Dinislam <dinis1333@mail.ru>
2023-08-03 22:49:38 +0500

    Изучай

--- lesson1.py
+++ lesson1.py
@@ -1,73 +1,132 @@
+# Игра Shmup - 3 часть
+# Cтолкновения и стрельба
 import pygame
-pygame.init()
+import random
 
-SIZE = WIDTH, HEIGHT = 1000, 800
-screen = pygame.display.set_mode(SIZE)
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+
+# Задаем цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+# Создаем игру и окно
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Shmup!")
 clock = pygame.time.Clock()
 
-class Car(pygame.sprite.Sprite):
-    def __init__(self, x, y, filename):
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(filename).convert_alpha()
-        self.rect = self.image.get_rect(center=(x, y))
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
 
-player=Car(400, 300, 'enemy.png')
-player.rect.y=300
-player.rect.x=400
-enemy=Car(100,300, 'enemy_car.jpg')
-enemy.rect.y=100
-enemy.rect.x=300
-game = True
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -8
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 8
+        self.rect.x += self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
 
-while game:
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 40))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(1, 8)
+        self.speedx = random.randrange(-3, 3)
+
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1, 8)
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10, 20))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = -10
+
+    def update(self):
+        self.rect.y += self.speedy
+        # убить, если он заходит за верхнюю часть экрана
+        if self.rect.bottom < 0:
+            self.kill()
+
+all_sprites = pygame.sprite.Group()
+mobs = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+for i in range(8):
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
+
+# Цикл игры
+running = True
+while running:
+    # Держим цикл на правильной скорости
+    clock.tick(FPS)
+    # Ввод процесса (события)
     for event in pygame.event.get():
+        # проверка для закрытия окна
         if event.type == pygame.QUIT:
-            game = False
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
 
-    screen.fill((0, 0, 0))
-    screen.blit(player.image, player.rect)
-    screen.blit(enemy.image,enemy.rect )
-    pygame.display.update()
-    clock.tick(15)
+    # Обновление
+    all_sprites.update()
 
-    if pygame.sprite.collide_mask(player,enemy):
-        game = False
+    hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
+    for hit in hits:
+        m = Mob()
+        all_sprites.add(m)
+        mobs.add(m)
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        player.rect.x -= 50
-    if keys[pygame.K_RIGHT]:
-        player.rect.x += 50
-    if keys[pygame.K_DOWN]:
-        player.rect.y += 50
-    if keys[pygame.K_UP]:
-        player.rect.y -= 50
+    # Проверка, не ударил ли моб игрока
+    hits = pygame.sprite.spritecollide(player, mobs, False)
+    if hits:
+        running = False
 
-    if keys[pygame.K_a]:
-        enemy.rect.x -= 50
-    if keys[pygame.K_d]:
-        enemy.rect.x += 50
-    if keys[pygame.K_s]:
-        enemy.rect.y += 50
-    if keys[pygame.K_w]:
-        enemy.rect.y -= 50
+    # Рендеринг
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    # После отрисовки всего, переворачиваем экран
+    pygame.display.flip()
 
-
-    if player.rect.y == 550:
-        player.rect.y -=50
-    if player.rect.y == 0:
-        player.rect.y +=50
-    if player.rect.x == 750:
-        player.rect.x -=50
-    if player.rect.x == 0:
-        player.rect.x +=50
-
-
-    if enemy.rect.y == 550:
-        enemy.rect.y -=50
-    if enemy.rect.y == 0:
-        enemy.rect.y +=50
-    if enemy.rect.x == 750:
-        enemy.rect.x -=50
-    if enemy.rect.x == 0:
-        enemy.rect.x +=50
+pygame.quit()
