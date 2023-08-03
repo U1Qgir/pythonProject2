f00ec19540e5e90bb91e1a32f8c571a5ccdbef08
Dinislam <dinis1333@mail.ru>
2023-08-03 22:36:17 +0500

    Реализовал удлинение змейки

--- snake.py
+++ snake.py
@@ -1,5 +1,10 @@
 import pygame
 from random import *
+
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(screen, blue, [x[0], x[1], snake_block, snake_block])
+
 pygame.init()
 
 SIZE = WIDTH, HEIGHT = 600, 400
@@ -7,26 +12,28 @@ screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
 
 blue=(0,0,255)
+green=(0,255,0)
 white=(255,255,255)
-game = True
-score = 0
+
+score = 1
+
 position=[300,200]
 moove=[0,0]
-W1=randint(10,580)
-H2=randint(10,380)
+foodpos = [200,100]
 block_size=10
+
+snake_list = []
+
+game = True
+
 while game:
+    clock.tick(30)
     score_font = pygame.font.Font(None, 35)
 
-    text_score=score_font.render(str(score),1,(255,255,245))
+    text_score=score_font.render(str(score-1),1,(255,255,245))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
-        if position[0]==W1 and position[1]==H2:
-            if event.pos[0] <= W1+20 and event.pos[1]<=H2+20 and event.pos[0] >= W1 and event.pos[1]>=H2:
-                W1 = randint(10, 600)
-                H2 = randint(10, 400)
-                score += 1
         elif event.type==pygame.KEYDOWN:
             if event.key==pygame.K_LEFT:
                 moove[0]= -block_size
@@ -42,14 +49,21 @@ while game:
                 moove[0] = 0
 
     screen.fill((0, 0, 0))
-    if W1 % 10 ==0 and H2 % 10==0:
-        pygame.draw.rect(screen,white,(W1,H2,10,10))
     pygame.draw.rect(screen,blue,(position[0],position[1],10,10))
+    pygame.draw.rect(screen,green,(foodpos[0],foodpos[1],10,10))
     screen.blit(text_score, (20, 30))
-
-    clock.tick(15)
-
+    snake_head = []
+    snake_head.append(position[0])
+    snake_head.append(position[1])
+    snake_list.append(snake_head)
+    if(len(snake_list) > score):
+        del snake_list[0]
+    our_snake(block_size,snake_list)
     pygame.display.update()
 
     position[0]+=moove[0]
     position[1]+=moove[1]
+    if(position[0] == foodpos[0] and position[1] == foodpos[1]):
+        foodpos[0] = (randint(0,WIDTH)//10)*10
+        foodpos[1] = (randint(0,HEIGHT)//10)*10
+        score+=1
